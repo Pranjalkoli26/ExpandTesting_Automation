350219a565e8f789285ed9dd8476ab3d84453d30
Pranjalkoli26 <147383051+Pranjalkoli26@users.noreply.github.com>
2024-03-04 10:24:46 +0530

    Update SearchButton.java

--- Expandtesting/src/main/java/org/example/SearchButton.java
+++ Expandtesting/src/main/java/org/example/SearchButton.java
@@ -6,4 +6,14 @@ public class SearchButton {
     public SearchButton(WebDriver driver) {
         this.driver = driver;
     }
+    By Examples=By.xpath("//*[@id="search-input"]");
+     By searchButton=By.xpath("//*[@id="search-button"]");
+
+
+     public RecordFound SearchAnExamples(String Examples)
+     {
+         driver.findElement(SearchAnExample).sendKeys(Examples);
+         driver.findElement(searchButton).click();
+         return new Examples(driver);
+     }
 }
